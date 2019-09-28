77b0d43531b37910a2c19e959146cb0e4a8c7ebb
JosephBadua <josephbadua1006@gmail.com>
2019-09-28 13:45:24 -0700

    animation

--- assets/java.js
+++ assets/java.js
@@ -18,15 +18,19 @@ $(document).ready(function() {
                 counter++;
                 $(".counter").text(counter + "%")
                 $("div.loader").css("border-right", "16px solid #3498db");
-            }  else if (counter < 150) {
+            }  else if (counter < 130) {
                 counter++
                 $("div.loader").css("border-left", "16px solid #3498db");
                 $("div.loader").animate({opacity: '0'}, "slow");
                 $(".hello").animate({opacity: '0'}, "slow");
                 $(".counter").animate({opacity: '0'}, "slow");
                 return;
-            }  else if(counter === 150) {
+            }  else if(counter === 130) {
                 $(".jumbotron").show();
-            }
-            },50);
+                $(".jumbotron").addClass("fade");
+                $(".title").addClass("entrance");
+                $(".name").addClass("tracking");
+                $(".preload").hide();
+            } 
+            },);
 });
