cd9ed60ab6c84398fe9d690046aae417dac27031
JosephBadua <josephbadua1006@gmail.com>
2019-09-28 01:10:10 -0700

    title page needs work

---
+++ assets/java.js
@@ -0,0 +1,32 @@
+let counter = 0;
+
+$(document).ready(function() {
+    $(".jumbotron").hide();
+        setInterval(function() {
+            if (counter < 25) {
+                counter++;
+                $(".counter").text(counter + "%");
+            } else if (counter < 50 ) {
+                counter++;
+                $(".counter").text(counter + "%")
+                $("div.loader").css("border-bottom", "16px solid #3498db");
+            } else if (counter < 75) {
+                counter++;
+                $(".counter").text(counter + "%")
+                $("div.loader").css("border-top", "16px solid #3498db");
+            } else if (counter < 100) {
+                counter++;
+                $(".counter").text(counter + "%")
+                $("div.loader").css("border-right", "16px solid #3498db");
+            }  else if (counter < 150) {
+                counter++
+                $("div.loader").css("border-left", "16px solid #3498db");
+                $("div.loader").animate({opacity: '0'}, "slow");
+                $(".hello").animate({opacity: '0'}, "slow");
+                $(".counter").animate({opacity: '0'}, "slow");
+                return;
+            }  else if(counter === 150) {
+                $(".jumbotron").show();
+            }
+            },50);
+});
