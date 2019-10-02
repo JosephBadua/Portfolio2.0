1e4f1ee00a8d6d89a2f2f5922f70a21588fe7c44
JosephBadua <josephbadua1006@gmail.com>
2019-10-02 14:52:39 -0700

    from scratch

--- assets/java.js
+++ assets/java.js
@@ -1,19 +1,8 @@
 let counter = 0;
 
 $(document).ready(function() {
-    $(".work").hide();
-    $(".Joseph").hide();
-    $(".all1").hide();
-    $(".section").hide();
-    $(".all").hide();
-    $(".picture").hide();
-    $(".jumbotron").hide();
-    $(".goRight").hide();
-    $(".borderRight").hide();
-    $(".descriptionText1").hide();
-    $(".descriptionText2").hide();
-    $(".descriptionText3").hide();
-    $(".descriptionText4").hide();
+    $(".new").hide();
+    $(".nameTitle").hide();
         setInterval(function() {
             if (counter < 25) {
                 counter++;
@@ -40,52 +29,11 @@ $(document).ready(function() {
             }  else if(counter < 180) {
                 console.log(counter);
                 counter++;
-                $(".jumbotron").show();
-                $(".jumbotron").addClass("fade");
-                $(".title").addClass("entrance");
-                $(".name").addClass("tracking");
+                $(".new").show();
+                $(".new").addClass("fade");
+                $(".nameTitle").show();
+                $(".nameTitle").addClass("fade");
                 $(".preload").hide();
-            }   else if(counter < 220) {
-                counter++;
-                $(".section").show();
-                $(".picture").show();
-                $(".descriptionText1").show();
-                $(".descriptionText1").addClass("fade1");
-            }   else if(counter < 260) {
-                $(".name").hide();
-                $(".sub-name").hide();
-                counter++;
-                $(".descriptionText2").show();
-                $(".descriptionText2").addClass("fade1");
-            }   else if(counter < 300) {
-                counter++;
-                $(".descriptionText3").show();
-                $(".descriptionText3").addClass("fade1");
-            }   else if(counter < 340) {
-                counter++;
-                $(".descriptionText4").show();
-                $(".descriptionText4").addClass("fade1");
-            }   else if(counter < 380) {
-                counter++;
-                $(".descriptionText1").addClass("out");
-                $(".descriptionText2").addClass("out");
-                $(".descriptionText3").addClass("out");
-                $(".descriptionText4").addClass("out");
-            }   else if(counter < 420) {
-                counter++;
-                $(".line").hide();
-                $(".goRight").show();
-                $(".goRight").addClass("swing");
-                $(".borderRight").show();
-                $(".all").show();
-                $(".all").addClass("fades");
-                $(".borderRight").addClass("fades");
-                $(".Joseph").show();
-                $(".all1").show();
-                $(".work").show();
-                $(".Joseph").addClass("scale-in");
-                $(".all1").addClass("scale-in");
-                $(".work").addClass("scale-in");
-            }  
-            }, 50);
+            }   
+            }, );
 });
